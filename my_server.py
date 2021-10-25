eb10cd5021dc1c55a01bb39de4c6c3bb13783f9b
Florence Bain <florence_bain@outlook.com>
2021-10-25 15:21:22 +0100

    added methods for the server

--- my_server.py
+++ my_server.py
@@ -7,54 +7,52 @@ class ConnectionPool:
         self.connection_pool = set()
 
     def send_welcome_message(self, writer):
-        message = dedent(f"""===(Welcome {writer.nickname}!
+        message = dedent(f"""
+        ===
+        (Welcome {writer.nickname}!
         There are {len(self.connection_pool) -1} user(s)
         here beside you
+
+        Help:
+        -Type anything to chat
+        -/list will list all the connected users
+        -/quit will disconnect you
         ===
         """)
 
         writer.write(f"{message}\n".encode())
 
-    def broadcat(self, write, message):
-        """
-        Broadcasts a general message to the entire pool
-        """
-        pass
+    def broadcat(self, writer, message):
+        for user in self.connection_pool:
+            if user != writer:
+                # no need to broadcast to the user sending the message
+                user.writer(f"{message}\n".encode())
 
     def broadcast_user_join(self, writer):
-        """
-        Calls the broadcast amthoad with a "user joining" message
-        """
-        pass
+        self.broadcast(writer, f"{writer.nickname} just joined")
 
     def broadcast_user_quit(self, writer):
-        """
-        Calls the broadcast method with a "user quitting" message
-        """
-        pass
+        self.broadcast(writer, f"{writer.nickname} just quit")
 
     def broadcast_new_message(self, writer, message):
-        """
-        Calls the broadcast method with a users chat message
-        """
-        pass
+        self.broadcast(writer, f"[{writer.nickname}] {message}")
 
     def list_users(self, writer):
-        """
-        Lists all the users in the pool
-        """
-        pass
+        message = "===\n"
+        message += "Currently connected users:"
+        for user in self.connection_pool:
+            if user == writer:
+                message += f"\n - {user.nickname} (you)"
+            else:
+                message += f"\n - {user.nickname}"
+
+        message += "\n==="
+        writer.write(f"{message}\n".encode())
 
     def add_new_user_to_pool(self, writer):
-        """
-        Adds a new user to the existing pool
-        """
         self.connection_pool.add(writer)
 
     def remove_user_from_pool(self, writer):
-        """
-        Removes an existing user from the pool
-        """
         self.connection_pool.remove(writer)
 
 
